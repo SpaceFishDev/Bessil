8e6b841f9e1f1bf1c5365330706c7192dd2dd98e
SpaceFishGameDev <ultragamesalot@gmail.com>
2022-10-18 12:50:24 +0200

    Fixed bessil main file

--- src/bessil.c
+++ src/bessil.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<bytecode.h>
+#include<lexer.h>
 
 int main(void){
-	compile_bytecode(STR(
+	lexer_info* lexer = init_lexer_info(STR(
 		"func main()\n"
 		"{\n"
 		"print(\"Hello, world!\");\n"
